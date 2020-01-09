483af8ab28bd1aa3ba3289f70e059c70b84ffb0a
Richard Kuo <rkuo2000@gmail.com>
2020-01-09 15:36:22 +0800

    Add files via upload

---
+++ scrape_accuweather.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+url = 'https://www.accuweather.com/en/tw/taipei-city/315078/weather-forecast/315078'
+
+driver = webdriver.Chrome(executable_path=r'C:\Users\RKUO\chromedriver.exe') # for Windows
+driver.get(url)
+soup = BeautifulSoup(driver.page_source, "html5lib")
+driver.close()
+
+item = soup.select('.temp')
+temp = item[0].text
+print("Current Temp:",temp.replace('\t','').replace('\n',''))
+
+item = soup.select('.real-feel')
+real = item[0].text
+print("Real-Feel:", real.replace('\t','').replace('\n',''))
+
+item = soup.select('.cond')
+cond = item[0].text
+print("Condition:",cond.replace('\t','').replace('\n',''))
